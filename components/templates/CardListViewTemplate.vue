8ea30d16ba89eb09ded53128ce198befe94351cd
Shun Yamazaki <syunn820@gmail.com>
2023-02-11 12:57:34 +0000

    update: layout
    max-width: 800pxに合わせたページサイズに修正

--- components/templates/CardListViewTemplate.vue
+++ components/templates/CardListViewTemplate.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
+    <v-col cols="12">
       <PageHeadline :title="prop.data.pageTitle" />
       <v-container>
         <WorkCard v-for="work in prop.data.works" :key="work.name" class="my-4" :work="work" />
