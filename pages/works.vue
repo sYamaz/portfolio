af618808ae75d2b1892a4bb47c5722aeb8e3c76b
Shun Yamazaki <syunn820@gmail.com>
2023-03-10 14:10:53 +0000

    add: item to works page

--- pages/works.vue
+++ pages/works.vue
@@ -17,6 +17,16 @@ const data: CardListViewTemplateData = {
   pageTitle: 'Works',
   works: [
     {
+      name: 'Youtube-to-speech',
+      img: getStaticImageUrl('youtube2speech.png'),
+      text: 'youtube-live comment reader. (Web speech API)',
+      platform: ['Web'],
+      status: 'Private',
+      url: '',
+      outerurl: '',
+      buttonName: ''
+    },
+    {
       name: 'RoutineTree',
       img: getStaticImageUrl('RoutineTree.png'),
       text: 'Task management application that helps you accomplish your daily routine tasks.',
