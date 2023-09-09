4bf3182870d186f60445f61c1b272459e0312cc7
Shun Yamazaki <syunn820@gmail.com>
2023-09-09 14:49:27 +0900

    seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' },
+        { hid: 'description', name: 'description', content: 'sYamazの自己紹介サイト' },
         { name: 'format-detection', content: 'telephone=no' }
       ],
       link: [
