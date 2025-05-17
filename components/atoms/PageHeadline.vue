5223cad012319de7b3038a59410df6a9e02df2f6
Shun Yamazaki <33094775+sYamaz@users.noreply.github.com>
2025-05-17 20:15:11 +0900

    renewal (#238)

--- components/atoms/PageHeadline.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>{{ prop.title }}</h1>
-</template>
-
-<script lang="ts" setup>
-
-const prop = defineProps<{title:string}>()
-
-</script>
--- components/atoms/SectionHeadline.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <h2>{{ prop.title }}</h2>
-</template>
-
-<script lang="ts" setup>
-const prop = defineProps<{title:string}>()
-</script>
